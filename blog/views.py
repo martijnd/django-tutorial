464c01163e40f44f271636c72bc255e699631f57
Martijn Dorsman <mdorsman@flaire.nl>
2020-02-13 22:10:01 +0100

    Part 6: User registration 13:48

--- blog/views.py
+++ blog/views.py
@@ -3,27 +3,12 @@ from __future__ import unicode_literals
 from django.http import HttpResponse
 
 from django.shortcuts import render
-
-posts = [
-    {
-        'author': 'Martijn Dorsman',
-        'title': 'Blog Post 1',
-        'content': 'First post content',
-        'date_posted': 'August 27, 2018'
-    },
-    {
-        'author': 'Jane Doe',
-        'title': 'Blog Post 2',
-        'content': 'Second post content',
-        'date_posted': 'August 28, 2018'
-    },
-]
+from .models import Post
 
 
 def home(request):
     context = {
-        'posts': posts,
-        'title': 'Home'
+        'posts': Post.objects.all(),
     }
     return render(request, 'blog/home.html', context)
 
---
+++ users/views.py
@@ -0,0 +1,9 @@
+# -*- coding: utf-8 -*-
+from __future__ import unicode_literals
+from django.contrib.auth.forms import UserCreationForm
+from django.shortcuts import render
+
+
+def register(request):
+    form = UserCreationForm()
+    return render(request, 'users/register.html', {'form': form})
